8166112b6986214f38f13d4b0b7259ba4fcb22e8
lucio2323232 <sgolikwolf@gmail.com>
2025-07-02 12:55:52 +0200

    Initial push

--- auto_push_github.bat
+++ auto_push_github.bat
@@ -3,6 +3,7 @@ git init
 git add .
 git commit -m "Initial push"
 git branch -M main
-git remote add origin https://github.com/YOUR_USER/english-trainer-final.git
+git remote add origin https://github.com/lucio2323232/trainer.git
 git push -u origin main
 pause
+
